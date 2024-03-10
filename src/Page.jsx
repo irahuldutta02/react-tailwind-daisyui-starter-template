cd8dd6e90b3cf14b957bfe4697cedee31762ab89
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-03-10 11:44:32 +0530

    .

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,8 +1,8 @@
 export function Page() {
   return (
     <>
-      <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-slate-900 flex-col gap-4 flex-wrap">
-        <div className="text-3xl flex justify-center items-center gap-4 font-bold text-orange-300 flex-wrap">
+      <div className="w-[100vw] h-[100vh]  flex justify-center items-center flex-col gap-4 flex-wrap">
+        <div className="text-3xl flex justify-center items-center gap-4 font-bold  flex-wrap">
           <span className="flex items-center justify-center gap-2">
             <span className="w-8">
               <img
@@ -24,8 +24,19 @@ export function Page() {
             </span>
             <span>TailwindCSS</span>
           </span>
+          <span>+</span>
+          <span className="flex items-center justify-center gap-2">
+            <span className="w-8">
+              <img
+                className="w-full"
+                src="https://daisyui.com/images/daisyui-logo/daisyui-logomark-1024-1024.png"
+                alt="DaisyUI"
+              />
+            </span>
+            <span>DaisyUI</span>
+          </span>
         </div>
-        <div className=" flex justify-center items-center gap-4 text-2xl text-orange-400 flex-wrap">
+        <div className=" flex justify-center items-center gap-4 text-2xl flex-wrap">
           <span>Powered by - Vite</span>
           <span className="w-6">
             <img
